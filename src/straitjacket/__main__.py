4d93c1fd568d7c7222a15c3a995e564e30324fe4
Manuel Barkhau <mbarkhau@gmail.com>
2019-04-02 09:36:56 +0200

    lint fix

--- src/straitjacket/__main__.py
+++ src/straitjacket/__main__.py
@@ -8,14 +8,14 @@
 import os
 import sys
 
+from . import sjfmt
+
+
 # To enable pretty tracebacks:
 #   echo "export ENABLE_BACKTRACE=1;" >> ~/.bashrc
 if os.environ.get('ENABLE_BACKTRACE') == "1":
     import backtrace
     backtrace.hook(align=True, strip_path=True, enable_on_envvar_only=True)
-
-
-from . import sjfmt
 
 
 if __name__ == '__main__':
