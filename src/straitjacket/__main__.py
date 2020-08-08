8ccf2df11b8cef635d58b48e9068b6d64113550a
Manuel Barkhau <mbarkhau@gmail.com>
2020-08-08 14:11:50 +0000

    remove backtrace

--- src/straitjacket/__main__.py
+++ src/straitjacket/__main__.py
@@ -10,14 +10,6 @@ import sys
 
 from . import sjfmt
 
-# To enable pretty tracebacks:
-#   echo "export ENABLE_BACKTRACE=1;" >> ~/.bashrc
-if os.environ.get('ENABLE_BACKTRACE') == '1':
-    import backtrace
-
-    backtrace.hook(align=True, strip_path=True, enable_on_envvar_only=True)
-
-
 if __name__ == '__main__':
     sjfmt.main()
     sys.exit(0)
